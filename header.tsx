059b7c417f245a290e462d515cf84efcc0a388cb
daanish08 <75127262+daanish08@users.noreply.github.com>
2024-11-12 15:15:18 +0530

    Add files via upload

---
+++ header.tsx
@@ -0,0 +1,17 @@
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>My Website</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Courses</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
