a3b338b918e3bbb81c043200e160e2e61b13c1d7
VIGNESH U <vignesh2002nathan@gmail.com>
2024-11-13 16:58:12 +0530

    updated context

--- src/App.tsx
+++ src/App.tsx
@@ -16,52 +16,58 @@ import PendingEnrollments from "./components/admin/PendingEnrollments";
 import CourseForm from "./components/courses/CourseForm";
 import UserDashboard from "./components/user/DashBoard";
 import UserEnrolledCourses from "./components/user/UserEnrolledCourses";
-import { AuthProvider } from "./components/contexts/authContext";
+import { AuthProvider } from "./contexts/authContext";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <AuthProvider>
-      <main className="mt-5 pt-2 bg-body-tertiary">
-        <Header />
-        {/* Routing configuration */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/admin/create-courses" element={<CourseForm />} />
-          <Route path="/courses/:id" element={<CourseDetails />} />
-          <Route path="/courses/update/:id" element={<CourseForm />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route
-            path="/admin/login"
-            element={<LoginComponent userType="admin" />}
-          />
+      <AuthProvider>
+        <main className="mt-5 pt-2 bg-body-tertiary">
+          <Header />
+          {/* Routing configuration */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/admin/create-courses" element={<CourseForm />} />
+            <Route path="/courses/:id" element={<CourseDetails />} />
+            <Route path="/courses/update/:id" element={<CourseForm />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route
+              path="/admin/login"
+              element={<LoginComponent userType="admin" />}
+            />
 
-          <Route
-            path="/admin/approve-courses"
-            element={<PendingEnrollments />}
-          />
+            <Route
+              path="/admin/approve-courses"
+              element={<PendingEnrollments />}
+            />
 
-          <Route path="/admin/users" element={<UserList />} />
-          <Route path="/admin/signup" element={<Signup userType="admin" />} />
-          <Route path="/user/signup" element={<Signup userType="user" />} />
-          <Route
-            path="/course/:id/success"
-            element={<GenerateCertificate userId="1" courseId="1" />}
-          />
-          <Route
-            path="/user/login"
-            element={<LoginComponent userType="user" />}
-          />
+            <Route path="/admin/users" element={<UserList />} />
+            <Route path="/admin/signup" element={<Signup userType="admin" />} />
+            <Route path="/user/signup" element={<Signup userType="user" />} />
+            <Route
+              path="/course/:id/success"
+              element={<GenerateCertificate userId="1" courseId="1" />}
+            />
+            <Route
+              path="/user/login"
+              element={<LoginComponent userType="user" />}
+            />
 
-          <Route path="/user/dashboard" element={<UserDashboard />} />
-          <Route path="/user/dashboard" element={<UserDashboard />} />
-          <Route path="/user/enroll-courses" element={<UserEnrolledCourses />} />
-        </Routes>
-      </main>
-      
-    </AuthProvider>
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route
+              path="/user/enroll-courses"
+              element={<UserEnrolledCourses />}
+            />
+            <Route
+              path="/user/enroll-courses"
+              element={<UserEnrolledCourses />}
+            />
+          </Routes>
+        </main>
+      </AuthProvider>
       <Footer />
     </BrowserRouter>
   );
